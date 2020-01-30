b3321d92148ff723e8af329faf73686af8adfa24
648800217 <648800217@qq.com>
2020-01-30 17:32:31 +0800

    5.0.0

--- js/backarticle.js
+++ js/backarticle.js
@@ -1,47 +1,20 @@
 $(function() {
-    let currentPage = 1;
-    let pageSize = 3;
+    // 初始化
+    // pagination( 总条数 )   每页 10 条
+    // $('#box').pagination( 56 );
 
-    function render() {
-        $.ajax({
-            url: "./setPage.php",
-            data: {
-                page: currentPage,
-                pageSize: pageSize
-            },
-            dataType: "json",
-            success: function(result) {
-                // 将数据渲染到页面
-                $("tbody").html(template("tableTemp", {
-                        item: result
-                    }))
-                    // 调用分页函数.参数:当前所在页, 总页数(用总条数 除以 每页显示多少条,在向上取整), ajax函数
-                setPage(currentPage, Math.ceil(result[0].size / pageSize), render)
-            }
-        })
-    }
-    render()
+    $('#box').pagination(20, {
+        // 各种配置
+        prev_text: "上一页",
+        next_text: "下一页",
+        items_per_page: 5, // 每页显示多少条
+        num_edge_entries: 1, // 配置首尾按钮显示的个数
+        num_display_entries: 5, // 配置连续主体显示的条目数
+        current_page: 4, // 当前页索引, 从0开始
+        load_first_page: false, // 表示一进入页面不需要直接调用一次回调函数
+        callback: function(index) {
+            alert(index);
+        }
+    })
 
-    /**
-     *
-     * @param pageCurrent 当前所在页
-     * @param pageSum 总页数
-     * @param callback 调用ajax
-     */
-    function setPage(pageCurrent, pageSum, callback) {
-        $(".pagination").bootstrapPaginator({
-            //设置版本号
-            bootstrapMajorVersion: 3,
-            // 显示第几页
-            currentPage: pageCurrent,
-            // 总页数
-            totalPages: pageSum,
-            //当单击操作按钮的时候, 执行该函数, 调用ajax渲染页面
-            onPageClicked: function(event, originalEvent, type, page) {
-                // 把当前点击的页码赋值给currentPage, 调用ajax,渲染页面
-                currentPage = page
-                callback && callback()
-            }
-        })
-    }
-})
+});
